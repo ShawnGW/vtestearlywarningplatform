af4e6a186968bd15882d4f0b90e4a1d9d2a18080
shawn <Mailsunguowei@163.com>
2019-08-07 12:00:02 +0800

    version 0.0.1 beta( perfect some hidden problems,but the exception of runtime is still not be fixed (can't catch the exception maybe aop))

--- src/main/java/com/vtest/it/vtestearlywarningplatform/controllers/UploadFiles.java
+++ src/main/java/com/vtest/it/vtestearlywarningplatform/controllers/UploadFiles.java
@@ -1,6 +1,8 @@
 package com.vtest.it.vtestearlywarningplatform.controllers;
 
+import com.alibaba.fastjson.JSON;
 import com.vtest.it.vtestearlywarningplatform.deal.TelPlatformDataDeal;
+import com.vtest.it.vtestearlywarningplatform.pojo.mes.MesConfigBean;
 import com.vtest.it.vtestearlywarningplatform.pojo.mes.SlotAndSequenceConfigBean;
 import com.vtest.it.vtestearlywarningplatform.pojo.rawdataBean.DealWaferIdInformationBean;
 import com.vtest.it.vtestearlywarningplatform.pojo.rawdataBean.RawdataInitBean;
@@ -41,6 +43,7 @@ public class UploadFiles {
         lot = lot.trim();
         ArrayList<File> sourceFiles=new ArrayList<>();
         Map<Boolean,String> result=new HashMap<>();
+        Map<String, String> errors = new HashMap<>();
         try {
             ArrayList<DealWaferIdInformationBean> dealWaferIdInformationBeanArrayList =new ArrayList<>();
             ArrayList<File> datFileList=new ArrayList<>();
@@ -68,87 +71,20 @@ public class UploadFiles {
                 result.put(false,"upload failed");
                 return  result;
             }
+            if (null == getMesInfor.getCustomerAndDeviceByLot(lot).getCustomerCode()) {
+                result.put(false, "this lot is not exist ,please check it again!");
+                return result;
+            }
             if (type.equals("normal")){
-                if (null == getMesInfor.getCustomerAndDeviceByLot(lot).getCustomerCode()) {
-                    result.put(false, "this lot is not exist ,please check it again!");
-                    return result;
-                }
-                SlotAndSequenceConfigBean slotAndSequenceConfigBean = getMesInfor.getLotSlotConfig(lot);
-                boolean slotFlag = false;
-                if (slotAndSequenceConfigBean.getReadType().toUpperCase().equals("SLOT")) {
-                    slotFlag = true;
-                }
-                for (File datFile : datFileList) {
-                    DealWaferIdInformationBean dealWaferIdInformationBean = new DealWaferIdInformationBean();
-                    dealWaferIdInformationBean.setNormalDieFlag(true);
-                    Map<String, String> information = getFileInformaton(datFile.getName());
-                    String waferId = information.get("waferId");
-                    String rightWaferId = waferId;
-                    if (slotFlag) {
-                        if (waferId.contains("-")) {
-                            try {
-                                String tempWaferId = getMesInfor.getWaferIdBySlot(lot, waferId.split("-")[1]);
-                                rightWaferId = "NA".equals(tempWaferId) ? rightWaferId : tempWaferId;
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                        } else {
-                            try {
-                                String tempWaferId = getMesInfor.getWaferIdBySlot(lot, waferId);
-                                rightWaferId = "NA".equals(tempWaferId) ? rightWaferId : tempWaferId;
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-                    RawdataInitBean rawdataInitBean = new RawdataInitBean();
-                    LinkedHashMap<String, String> dataProperties = new LinkedHashMap<>();
-                    dataProperties.put("Wafer ID", rightWaferId);
-                    dataProperties.put("Operator", "V888");
-                    dataProperties.put("CP Process", cpProcess.trim());
-                    dataProperties.put("Lot ID", lot);
-                    rawdataInitBean.setDataProperties(dataProperties);
-                    dealWaferIdInformationBean.setRawdataInitBean(rawdataInitBean);
-                    dealWaferIdInformationBean.setFile(datFile);
-                    dealWaferIdInformationBeanArrayList.add(dealWaferIdInformationBean);
-                }
+                dealDatUploadFile(dealWaferIdInformationBeanArrayList, datFileList, cpProcess, lot, errors);
             }else {
-                Map<String, Map<String, String>> waferIdTestTimeInformation = new HashMap<>();
-                for (File daFile : daFileList) {
-                    try {
-                        HashMap<String, String> resultMap = telOpusProberMappingDaParse.GetResult(daFile);
-                        Map<String, String> information = getFileInformaton(daFile.getName());
-                        String waferId = information.get("waferId");
-                        waferIdTestTimeInformation.put(waferId, resultMap);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-                for (File rmpFile : rmpFileList) {
-                    DealWaferIdInformationBean dealWaferIdInformationBean = new DealWaferIdInformationBean();
-                    dealWaferIdInformationBean.setNormalDieFlag(false);
-                    RawdataInitBean rawdataInitBean = new RawdataInitBean();
-                    Map<String, String> information = getFileInformaton(rmpFile.getName());
-                    String waferId = information.get("waferId");
-
-                    String testStartTime = waferIdTestTimeInformation.get(waferId).get("testStartTime");
-                    String testEndTime = waferIdTestTimeInformation.get(waferId).get("testEndTime");
-                    testStartTime = null == testStartTime ? simpleDateFormatOthers.format(new Date()) : testStartTime;
-                    testEndTime = null == testEndTime ? simpleDateFormatOthers.format(new Date()) : testEndTime;
-                    LinkedHashMap<String, String> dataProperties = new LinkedHashMap<>();
-                    dataProperties.put("Wafer ID", waferId);
-                    dataProperties.put("Operator", "V888");
-                    dataProperties.put("CP Process", cpProcess);
-                    dataProperties.put("Test Start Time", testStartTime);
-                    dataProperties.put("Test End Time", testEndTime);
-                    dataProperties.put("Lot ID", lot);
-                    rawdataInitBean.setDataProperties(dataProperties);
-                    dealWaferIdInformationBean.setRawdataInitBean(rawdataInitBean);
-                    dealWaferIdInformationBean.setFile(rmpFile);
-                    dealWaferIdInformationBeanArrayList.add(dealWaferIdInformationBean);
-                }
+                dealRpmUploadFile(dealWaferIdInformationBeanArrayList, daFileList, rmpFileList, cpProcess, lot, errors);
             }
             telPlatformDataDeal.deal(dealWaferIdInformationBeanArrayList);
+            if (errors.size() > 0) {
+                result.put(false, JSON.toJSONString(errors));
+                return result;
+            }
             result.put(true,"generate success");
         }catch (Exception e){
             result.put(false,"generate failed :"+e.getMessage());
@@ -164,6 +100,86 @@ public class UploadFiles {
         return result;
     }
 
+    public void dealDatUploadFile(ArrayList<DealWaferIdInformationBean> dealWaferIdInformationBeanArrayList, ArrayList<File> datFileList, String cpProcess, String lot, Map<String, String> errors) {
+        SlotAndSequenceConfigBean slotAndSequenceConfigBean = getMesInfor.getLotSlotConfig(lot);
+        boolean slotFlag = false;
+        if (slotAndSequenceConfigBean.getReadType().toUpperCase().equals("SLOT")) {
+            slotFlag = true;
+        }
+        for (File datFile : datFileList) {
+            DealWaferIdInformationBean dealWaferIdInformationBean = new DealWaferIdInformationBean();
+            dealWaferIdInformationBean.setNormalDieFlag(true);
+            Map<String, String> information = getFileInformaton(datFile.getName());
+            String waferId = information.get("waferId");
+            String rightWaferId = waferId;
+            if (slotFlag) {
+                try {
+                    String tempWaferId = getMesInfor.getWaferIdBySlot(lot, waferId.substring(waferId.length() - 2));
+                    rightWaferId = "NA".equals(tempWaferId) ? rightWaferId : tempWaferId;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            MesConfigBean mesConfigBean = getMesInfor.getWaferConfigFromMes(rightWaferId, cpProcess.trim());
+            if (null == mesConfigBean.getInnerLot()) {
+                errors.put(waferId + " : " + rightWaferId, "can't find this wafer in mes system : no such wafer or cpProcess");
+            } else {
+                RawdataInitBean rawdataInitBean = new RawdataInitBean();
+                LinkedHashMap<String, String> dataProperties = new LinkedHashMap<>();
+                dataProperties.put("Wafer ID", rightWaferId);
+                dataProperties.put("Operator", "V888");
+                dataProperties.put("CP Process", cpProcess.trim());
+                dataProperties.put("Lot ID", lot);
+                rawdataInitBean.setDataProperties(dataProperties);
+                dealWaferIdInformationBean.setRawdataInitBean(rawdataInitBean);
+                dealWaferIdInformationBean.setFile(datFile);
+                dealWaferIdInformationBeanArrayList.add(dealWaferIdInformationBean);
+            }
+        }
+    }
+
+    public void dealRpmUploadFile(ArrayList<DealWaferIdInformationBean> dealWaferIdInformationBeanArrayList, ArrayList<File> daFileList, ArrayList<File> rmpFileList, String cpProcess, String lot, Map<String, String> errors) {
+        Map<String, Map<String, String>> waferIdTestTimeInformation = new HashMap<>();
+        for (File daFile : daFileList) {
+            try {
+                HashMap<String, String> resultMap = telOpusProberMappingDaParse.GetResult(daFile);
+                Map<String, String> information = getFileInformaton(daFile.getName());
+                String waferId = information.get("waferId");
+                waferIdTestTimeInformation.put(waferId, resultMap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        for (File rmpFile : rmpFileList) {
+            DealWaferIdInformationBean dealWaferIdInformationBean = new DealWaferIdInformationBean();
+            dealWaferIdInformationBean.setNormalDieFlag(false);
+            RawdataInitBean rawdataInitBean = new RawdataInitBean();
+            Map<String, String> information = getFileInformaton(rmpFile.getName());
+            String waferId = information.get("waferId");
+
+            String testStartTime = waferIdTestTimeInformation.get(waferId).get("testStartTime");
+            String testEndTime = waferIdTestTimeInformation.get(waferId).get("testEndTime");
+            testStartTime = null == testStartTime ? simpleDateFormatOthers.format(new Date()) : testStartTime;
+            testEndTime = null == testEndTime ? simpleDateFormatOthers.format(new Date()) : testEndTime;
+
+            MesConfigBean mesConfigBean = getMesInfor.getWaferConfigFromMes(waferId, cpProcess.trim());
+            if (null == mesConfigBean.getInnerLot()) {
+                errors.put(waferId, "can't find this wafer in mes system : no such wafer or cpProcess");
+            } else {
+                LinkedHashMap<String, String> dataProperties = new LinkedHashMap<>();
+                dataProperties.put("Wafer ID", waferId);
+                dataProperties.put("Operator", "V888");
+                dataProperties.put("CP Process", cpProcess);
+                dataProperties.put("Test Start Time", testStartTime);
+                dataProperties.put("Test End Time", testEndTime);
+                dataProperties.put("Lot ID", lot);
+                rawdataInitBean.setDataProperties(dataProperties);
+                dealWaferIdInformationBean.setRawdataInitBean(rawdataInitBean);
+                dealWaferIdInformationBean.setFile(rmpFile);
+                dealWaferIdInformationBeanArrayList.add(dealWaferIdInformationBean);
+            }
+        }
+    }
     public String getFileNameAfterModify(String fileName) {
         Map<String, String> information = getFileInformaton(fileName);
         return information.get("waferId") + "1" + information.get("suffix");
